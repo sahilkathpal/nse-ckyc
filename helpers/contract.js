dd285211bf7aac273e5aaa3782a94e68056973d2
sahil <sahil@esscale.co>
2016-07-27 16:07:28 +0530

    Fixed promise bug in contracts

--- helpers/contract.js
+++ helpers/contract.js
@@ -1,14 +1,20 @@
 var User = require('../models/User');
 module.exports = function () {
   var erisdbURL = "http://localhost:1337/rpc";
-  return User.findOne({role: 9})
-  .then(function (nse) {
-    var accountData = {address: nse.address, pubKey: nse.pub_key, privKey: nse.priv_key};
-    var contractData = require('../../.eris/apps/nse-ckyc/epm.json');
-    var kycContractAddress = contractData["deployV1"];
-    var kycAbi = JSON.parse(fs.readFileSync("../../.eris/apps/nse-ckyc/abi/" + kycContractAddress));
-    var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData);
-    var contract = contractsManager.newContractFactory(kycAbi).at(kycContractAddress);
-    return contract;
-  })
+  var promise = new Promise (function (resolve, reject) {
+    User.findOne({role: 9})
+    .then(function (nse) {
+      var accountData = {address: nse.address, pubKey: nse.pub_key, privKey: nse.priv_key};
+      var contractData = require('../../.eris/apps/nse-ckyc/epm.json');
+      var kycContractAddress = contractData["deployV1"];
+      var kycAbi = JSON.parse(fs.readFileSync("../../.eris/apps/nse-ckyc/abi/" + kycContractAddress));
+      var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData);
+      var contract = contractsManager.newContractFactory(kycAbi).at(kycContractAddress);
+      resolve(contract);
+    })
+    .catch(function (err) {
+      reject(err);
+    });
+  });
+  return promise;
 }
