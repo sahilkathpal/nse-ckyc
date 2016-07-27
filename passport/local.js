1c0e5285c6fcba525a2008e6c40e597e3e2c933c
sahil <sahil@esscale.co>
2016-07-28 02:04:44 +0530

    dsdsa

--- passport/local.js
+++ passport/local.js
@@ -3,32 +3,30 @@ module.exports = function (passport, LocalStrategy) {
   var User = require('../models/User.js');
   var bCrypt = require('bcrypt');
 
-  passport.use('login', new LocalStrategy({
-    passReqToCallback : true
-  },
-  function(req, username, password, done) {
-    // check in mongo if a user with username exists or not
-    console.log(req.body, username, password);
-    User.findOne({ 'email' :  req.body.email },
-      function(err, user) {
-        console.log(err, user);
-        // In case of any error, return using the done method
-        if (err)
-          return done(err);
-        // Username does not exist, log error & redirect back
-        if (!user){
-          return done('User Not Found', false);
+  passport.use('login', new LocalStrategy({ passReqToCallback : true, usernameField: 'email', passwordField: 'password' }, function(req, email, password, done) {
+      // check in mongo if a user with username exists or not
+      console.log(req.body, email, password);
+      User.findOne({ 'email':  email },
+        function(err, user) {
+          console.log(err, user);
+          // In case of any error, return using the done method
+          if (err)
+            return done(err);
+          // Username does not exist, log error & redirect back
+          if (!user){
+            return done('User Not Found', false);
+          }
+          // User exists but wrong password, log the error
+          if (!isValidPassword(user, password)){
+            return done("Invalid Password", false);
+          }
+          // User and password both match, return user from
+          // done method which will be treated like success
+          return done(null, user);
         }
-        // User exists but wrong password, log the error
-        if (!isValidPassword(user, req.body.password)){
-          return done("Invalid Password", false);
-        }
-        // User and password both match, return user from
-        // done method which will be treated like success
-        return done(null, user);
-      }
-    );
-  }));
+      );
+    })
+  );
 
   function isValidPassword(user, password) {
     var result =  bCrypt.compareSync(password, user.password);
