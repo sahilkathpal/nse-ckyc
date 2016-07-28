27d614c6c77c2b9807bf67292f1595dfe75d821a
sahil <sahil@esscale.co>
2016-07-28 14:04:46 +0530

    api gateway

--- routes/load-balance.js
+++ routes/load-balance.js
@@ -1,5 +1,6 @@
 module.exports = function () {
-  const router = require('express').Router()
+  var router = require('express').Router();
+
   var user = require('../controllers/userController')();
 
   router.post('/customers/fetch', user.fetchForBank);
